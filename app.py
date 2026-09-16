6b7fdbe0f20d894d63d87e0ccd2467b6a7b144a3
l3cky77 <tejashsomu41@gmail.com>
2026-09-17 00:19:40 +0530

    Refactor Flask routes and remove unused imports

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def home():
     return render_template("codewithdino.html")
+
+
 @app.route("/course")
 def course():
     return render_template("courses.html")
+
+
 @app.route("/home")
 def about():
     return render_template("codewithdino.html")
+
+
 @app.route("/roadmap")
 def roadmap():
     return render_template("roadmap.html")
+
+
 @app.route("/aibot")
 def aibot():
     return render_template("dinoai.html")
 
 
-
-
 if __name__ == "__main__":
-
     app.run(
         host="0.0.0.0",
         port=5000,
